af8942599c8d12a89ec877d8bbad058e5f7a9fe3
Ilia Baryshnikov <qwelias@gmail.com>
2023-09-24 23:56:30 +0200

    fix: update gnome-shell, todo libadwaita

--- patch.sh
+++ patch.sh
@@ -7,24 +7,24 @@ set -o nounset
 shopt -s globstar
 shopt -s nullglob
 # set -o xtrace
+
 tab() { sed 's/^/  /'; }
 dir=$(dirname "$(readlink -f "$0")")
 
 setup () {
     [ -d gnome-shell ] || (\
-        git clone --depth 1 --branch 44.0 https://gitlab.gnome.org/GNOME/gnome-shell.git && \
+        git clone --depth 1 --branch 44.5 https://gitlab.gnome.org/GNOME/gnome-shell.git gnome-shell && \
         git -C gnome-shell apply ../gnome-shell.patch
     )
-    [ -d gtk ] || (\
-        git clone --depth 1 --branch gtk-3-24 https://gitlab.gnome.org/GNOME/gtk.git && \
-        git -C gtk apply ../gtk.patch
+    [ -d gtk3 ] || (\
+        git clone --depth 1 --branch gtk-3-24 https://gitlab.gnome.org/GNOME/gtk.git gtk3 && \
+        git -C gtk3 apply ../gtk.patch
     )
     [ -d libadwaita ] || (\
-        git clone --depth 1 --branch 1.3.1 https://gitlab.gnome.org/GNOME/libadwaita.git && \
+        git clone --depth 1 --branch 1.4.0 https://gitlab.gnome.org/GNOME/libadwaita.git libadwaita && \
         git -C libadwaita apply ../libadwaita.patch
     )
 }
-
 
 gnomeshell () {
     target=/usr/share/gnome-shell/theme/qwelias.css
@@ -32,15 +32,15 @@ gnomeshell () {
     rm -rf gnome-shell.css
     sassc -a gnome-shell.scss gnome-shell.css
     sudo cp gnome-shell.css $target
-    gnome-shell-extension-tool -d gnome-core@qwelias.me
-    gnome-shell-extension-tool -e gnome-core@qwelias.me
+    gnome-extensions disable gnome-core@qwelias.me
+    gnome-extensions enable gnome-core@qwelias.me
 }
 
 gtk3 () {
     target=~/.local/share/themes/qwelias
     sudo rm -rf $target/*
     sudo mkdir -p $target/gtk-3.0
-    cd $dir/gtk/gtk/theme/Adwaita
+    cd $dir/gtk3/gtk/theme/Adwaita
     ./parse-sass.sh
     sudo cp gtk-contained-dark.css $target/gtk-3.0/gtk.css
     sudo cp gtk-contained-dark.css $target/gtk-3.0/gtk-dark.css
@@ -54,6 +54,10 @@ libadwaita () {
     meson --prefix /usr . _build
     ninja -C _build
     ninja -C _build install
+}
+
+clear () {
+    rm -rf gnome-shell gtk3 libadwaita
 }
 
 if [ $# -eq 0 ]
